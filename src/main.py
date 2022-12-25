2e0077e8f08087b03791bd3401f7e67c0b74b058
Elliot Jones <elliottj@tutanota.com>
2022-12-24 18:39:44 -0800

    Feat: format main.py
    Formats 'main.py' to improve multi-line sting and function
    readability.

--- src/main.py
+++ src/main.py
@@ -4,24 +4,35 @@ from town import Town
 from character import character
 from TextGenerator import stream_print
 
+
 def main():
-    print("Welcome to Argyle, please input your name")
-    character_name = input()
+    character_name = input("Welcome to Argyle, please input your name\n")
+    town_name = "Sorenthal"
+
     if character_name:
         print("Thank you.") 
         player_character = character(name=character_name)
     else:
         print("No name specified. Generating random character...\n")
         player_character = character()
-    
-    town_name = "Sorenthal"
-    my_town = generate_town(town_name, True, True)
+
+    my_town = generate_town(
+        town_name,
+        tournament=True,
+        melee=True,
+    )
 
     while(True):
-        print("\n")
-        print(f"Welcome to {town_name}, {player_character.name}. " + \
-              f"You stand in the town square.")
-        describe_town(my_town, player_character.name)
+        print(\
+            f"\nWelcome to {town_name}, {player_character.name}." + \
+            "You stand in the town square."
+        )
+
+        describe_town(
+            my_town, 
+            player_character.name
+        )
+
         response = input()
        
         #FIXME
@@ -40,61 +51,108 @@ def main():
             print("\n============= not yet implemented, sorry ==============\n")
             continue
 
-def go_jousting(combatant_A_name="", combatant_B_name=""):
-    new_joust = joust.Joust(combatant_A_name, combatant_B_name)
+def go_jousting(
+        combatant_A_name="",
+        combatant_B_name="",
+):
+    new_joust = joust.Joust(
+        combatant_A_name,
+        combatant_B_name,
+    )
+
     new_joust.main_joust()
 
 def go_melee(name=""):
     melee.main_melee(name)
 
-def generate_town(town_name, tournament, melee):
-    town = Town(name=town_name, tournament=tournament, \
-                      melee=melee)
+def generate_town(
+    town_name, 
+    tournament, 
+    melee
+):
+    town = Town(
+        name=town_name,
+        tournament=tournament,
+        melee=melee,
+    )
+
     return town
 
-def describe_town(town, player_name):
+def describe_town(
+    town, 
+    player_name
+):
     town.describe_town(player_name)
-    print("\n\n=========== Actions =============")
-    print("[0] = Describe Character")
-    print("[1] = Visit the jousting tournament")
-    print("[2] = Visit the grand melee")
-    print("[3] = Visit the Tavern")
-    print("[4] = Explore the forest")
-    print("[5] = Visit the Mage Tower")
-    print("[6] = Visit the Night Market")
-    print("[7] = Explore the Underbelly")
-    print("[8] = Visit the Palace")
-    print("[9] = More...")
 
-def test_character_status(name="test-name", character_class="", description=""):
+    actions = \
+        "\n" + \
+        "\n=========== Actions =============" + \
+        "\n[0] = Describe Character" + \
+        "\n[1] = Visit the jousting tournament" + \
+        "\n[2] = Visit the grand melee" + \
+        "\n[3] = Visit the Tavern" + \
+        "\n[4] = Explore the forest" + \
+        "\n[5] = Visit the Mage Tower" + \
+        "\n[6] = Visit the Night Market" + \
+        "\n[7] = Explore the Underbelly" + \
+        "\n[8] = Visit the Palace" + \
+        "\n[9] = More..."_
+
+    print(actions)
+
+def test_character_status(
+    name="test-name",
+    character_class="",
+    description=""
+):
     print("Now engaging character status test. Please stand by.\n")
     print("================== generating character ======================")
-    test_character = character(name, character_class, description)
+    test_character = character(
+        name,
+        character_class,
+        description
+    )
     print("================== printing test character ===================")
     print(test_character)
     print("================== description ===============================")
     print(test_character.description)
     print("================== status ====================================")
     print(test_character.status)
+
     return test_character
 
 def test_melee():
     print("Now engaging melee test. Please stand by.")
-    user_character = test_character_status(name="user", \
-                                           character_class="commoner", \
-                                           description="unremarkable")
+    user_character = test_character_status(
+        name="user",
+        character_class="commoner",
+        description="unremarkable",
+    )
+
     comp_character = test_character_status(name="comp")
-    my_town = generate_town("Sorenthal", True, True)
+    my_town = generate_town(
+        town_name="Sorenthal",
+        tournament=True,
+        melee=True
+    )
+
     print("Testing melee now...:")
     my_melee = melee.melee()
-    my_melee.test_melee(user_character, comp_character)
+    my_melee.test_melee(
+        user_character,
+        comp_character
+    )
 
 def test_town():
     print("This is a test of the town functionality.")
     print("Please stand by as a town is generated.")
 
     town_name = "Sorenthal"
-    my_town = generate_town(town_name, True, True)
+    my_town = generate_town(
+        town_name="Sorenthal",
+        tournament=True,
+        melee=True
+    )
 
     while(True):
         print(f"Welcome to {town_name}. You stand in the town square.")
@@ -104,6 +162,7 @@ def test_town():
 def test_joust():
     print("This is a test of the joust functionality.")
     print("Please stand by as a joust is generated.")
+
     go_jousting("Aethelric, the strong")
 
 #FIXME
@@ -112,11 +171,11 @@ def test_clock():
         break
 
 
-
-
 main()
+
 #test_character_status
 #test_melee()
 #test_town()
 #test_joust()
 #test_clock()
+
